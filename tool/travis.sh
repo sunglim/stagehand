61c809aac8486ee5dbeadcbb0a656a14ed971a1f
sungguk <limasdf@gmail.com>
2014-12-24 16:50:10 +0900

    fWIP

--- tool/travis.sh
+++ tool/travis.sh
@@ -18,7 +18,7 @@ export PATH="$DART_SDK/bin:$PATH"
 # Display installed versions.
 dart --version
 
-# Globally install grinder.
+# Globally install grinder, test_runner.
 pub global activate grinder
 pub global activate test_runner
 export PATH=~/.pub-cache/bin:$PATH
@@ -32,16 +32,13 @@ dartanalyzer --fatal-warnings \
   lib/stagehand.dart \
   test/all_test.dart
 
-# Run the tests.
-dart test/all_test.dart
-
-# Get the Dart SDK.
+# Get the Dart content shell.
 DART_CONTENT_SHELL=content_shell-linux-x64-release.zip
 curl http://storage.googleapis.com/dart-archive/channels/stable/release/latest/dartium/content_shell-linux-x64-release.zip > $DART_CONTENT_SHELL
 unzip $DART_CONTENT_SHELL > /dev/null
 rm $DART_CONTENT_SHELL
 
-run_tests --content-shell-bin ./drt-lucid64-full-stable-42039.0/content_shell test/all_test.dart
+run_tests --content-shell-bin ./drt-lucid64-full-stable-42039.0/content_shell test/all_test.dart test/web_test.dart
 
 # Run all the generators and analyze the generated code.
 grind test
